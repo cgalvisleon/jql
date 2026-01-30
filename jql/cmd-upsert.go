3b579e4112f34a9ad41cd2768e7e6901f7dea92c
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-01-30 14:57:41 -0500

    Backup

--- jql/cmd-upsert.go
+++ jql/cmd-upsert.go
@@ -3,32 +3,25 @@ package jql
 import "github.com/cgalvisleon/et/et"
 
 func (s *Cmd) upsert() (et.Items, error) {
-	if s.Model == nil {
+	if len(s.Data) == 0 {
 		return et.Items{}, nil
 	}
 
+	data := s.Data[0]
 	from := s.Model
-	for _, data := range s.Data {
-		exists, err := from.
-			ItExists(data).
-			ItExists()
-		if err != nil {
-			return et.Items{}, err
-		}
-
-		if exists {
-			s.Type = UPDATE
-			s.WhereByPrimaryKeys(data)
-			return s.update()
-		}
+	exists, err := from.
+		ItExists(data).
+		ItExists()
+	if err != nil {
+		return et.Items{}, err
 	}
 
 	if exists {
-		s.Type = TypeUpdate
-		s.WhereByPrimaryKeys(data)
+		s.Type = UPDATE
+		s.Wheres.byPk(from, data)
 		return s.update()
 	}
 
-	s.Type = TypeInsert
+	s.Type = INSERT
 	return s.insert()
 }
