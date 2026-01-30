3b044b8282ad33438cc8c1a1c6f0a13efec4083c
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-01-30 14:34:54 -0500

    Backup

--- jql/cmd.go
+++ jql/cmd.go
@@ -98,6 +98,48 @@ func (s *Cmd) setTx(tx *Tx) *Cmd {
 }
 
 /**
+* insert
+* @return et.Items, error
+**/
+func (s *Cmd) insert() (et.Items, error) {
+	result := et.Items{}
+	for _, new := range s.Data {
+		old := et.Json{}
+		for _, fn := range s.beforeInserts {
+			err := fn(s.tx, old, new)
+			if err != nil {
+				return et.Items{}, err
+			}
+		}
+
+		if new.IsEmpty() {
+			continue
+		}
+
+		result, err := s.db.Command(s)
+		if err != nil {
+			return et.Items{}, err
+		}
+
+		if !result.Ok {
+			continue
+		}
+
+		new = result.First().Result
+		for _, fn := range s.afterInserts {
+			err := fn(s.tx, old, new)
+			if err != nil {
+				return et.Items{}, err
+			}
+		}
+
+		result.Add(new)
+	}
+
+	return result, nil
+}
+
+/**
 * ExecTx
 * @param tx *Tx
 * @return et.Items, error
