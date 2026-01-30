069529ebf92bf1ccd01a7d0efb868ed66347aba2
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-01-30 14:58:06 -0500

    Backup

--- jql/cmd.go
+++ jql/cmd.go
@@ -240,6 +240,34 @@ func (s *Cmd) delete() (et.Items, error) {
 }
 
 /**
+* upsert
+* @return et.Items, error
+**/
+func (s *Cmd) upsert() (et.Items, error) {
+	if len(s.Data) == 0 {
+		return et.Items{}, nil
+	}
+
+	data := s.Data[0]
+	from := s.Model
+	exists, err := from.
+		ItExists(data).
+		ItExists()
+	if err != nil {
+		return et.Items{}, err
+	}
+
+	if exists {
+		s.Type = UPDATE
+		s.Wheres.byPk(from, data)
+		return s.update()
+	}
+
+	s.Type = INSERT
+	return s.insert()
+}
+
+/**
 * ExecTx
 * @param tx *Tx
 * @return et.Items, error
