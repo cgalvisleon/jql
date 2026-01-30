a3aec1e2d5a01dc63d05339f96d4f2c4b7e2213f
Cesar Galvis Leon <cgalvisleon@gmail.com>
2026-01-30 15:12:19 -0500

    Backup

--- jql/core.go
+++ jql/core.go
@@ -1,5 +1,69 @@
 package jql
 
+var models *Model
+
+/**
+* defineModel
+* @param db *DB
+* @return error
+**/
+func defineModel(db *DB) error {
+	if models != nil {
+		return nil
+	}
+
+	var err error
+	models, err = db.NewModel("core", "models", 1)
+	if err != nil {
+		return err
+	}
+
+	models, err = db.NewModel(et.Json{
+		"schema":  "core",
+		"name":    "models",
+		"version": 1,
+		"columns": []et.Json{
+			{
+				"name": "created_at",
+				"type": "datetime",
+			},
+			{
+				"name": "updated_at",
+				"type": "datetime",
+			},
+			{
+				"name": "name",
+				"type": "text",
+			},
+			{
+				"name": "version",
+				"type": "int",
+			},
+			{
+				"name": "definition",
+				"type": "bytes",
+			},
+			{
+				"name": IDX,
+				"type": "key",
+			},
+		},
+		"record_field": IDX,
+		"primary_keys": []string{"name"},
+		"indexes":      []string{"version", IDX},
+		"is_core":      true,
+	})
+	if err != nil {
+		return err
+	}
+
+	if err = models.Init(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (s *DB) initCore() error {
 	if err := defineModel(s); err != nil {
 		return err
